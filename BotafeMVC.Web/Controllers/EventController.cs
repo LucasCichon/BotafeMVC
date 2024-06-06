237c94ba6ebdacd87976ecc7f615502bb4f65456
Lucas Cichon <lucas.cichon93@gmail.com>
2024-06-06 09:39:38 +0200

    first changes

---
+++ BotafeMVC.Web/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using BotafeMVC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotafeMVC.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        [HttpGet]
+        public IActionResult Events()
+        {
+            ViewData["Test"] = "ViewDataTest";
+
+            List<BotafeEvent> events = new List<BotafeEvent>();
+            events.Add(new BotafeEvent() { Id = 1, Name = "Ławka", StartDate = new DateOnly(2024, 7, 13), EndDate = new DateOnly(2024, 7, 17), PlacesAvailable = 15, TotalNumberOfPlaces = 50 });
+            events.Add(new BotafeEvent() { Id = 2, Name = "Lectio Divina", StartDate = new DateOnly(2024, 8, 2), EndDate = new DateOnly(2024, 9, 2), PlacesAvailable = 2, TotalNumberOfPlaces = 8 });
+            events.Add(new BotafeEvent() { Id = 3, Name = "Akrobatyka Małżeńska", StartDate = new DateOnly(2024, 10, 12), EndDate = new DateOnly(2024, 10, 25), PlacesAvailable = 7, TotalNumberOfPlaces = 30 });
+
+            return View(events);
+        }
+    }
+}
---
+++ BotafeMVC.Web/Models/BotafeEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace BotafeMVC.Web.Models
+{
+    public class BotafeEvent
+    {
+        [DisplayName("Identyfikator")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public int PlacesAvailable { get; set; }
+        public int TotalNumberOfPlaces { get; set; }
+
+        //// jeżeli nie będzie '?', to usuwając Ownera, automatycznie wszyskie jego Eventy zostaną usunięte...
+        //public int? EventOwnerId { get; set; }
+        //public EventOwner EventOwner { get; set; }
+        //public List<Enrollment> Enrollments { get; set; } = new();
+    }
+}
